967353312217883d72cc08234f6669def283101b
iuliadeko14 <52921203+iuliadeko14@users.noreply.github.com>
2021-02-05 09:09:27 -0300

    Delete index.jsx

--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import *as ReactDom from 'react-dom'
-
-window.addEventListener('load', () => {
-ReactDom.render(<Header />, document.getElementById('react-root'))
-})
